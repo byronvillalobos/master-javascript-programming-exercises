1a058200565037a6d11fe11d68715756c98ec374
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-24 22:39:23 +0000

    delivered

--- exercises/05-getProperty/app.js
+++ exercises/05-getProperty/app.js
@@ -1,3 +1,15 @@
+var car = {
+  Marca: 'Toyota',
+  Model:'Fortuner'
+};
+
+
 function getProperty(obj, key) {
   // your code here
+    return obj[key];
+
 }
+
+var output = getProperty(car, 'Marca');
+console.log(output); // --> 'value'
+
--- exercises/06-addProperty/app.js
+++ exercises/06-addProperty/app.js
@@ -1,3 +1,16 @@
+var myObj  = {
+  Marca: 'Toyota',
+
+};
+
+
 function addProperty(obj, key) {
   // your code here
+    obj[key] = 'Fortuner'
+    return obj
 }
+
+
+addProperty(myObj, 'Modelo');
+console.log(myObj.myProperty); // --> true
+console.log(myObj); // --> true
